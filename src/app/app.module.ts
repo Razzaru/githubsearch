1d26329f1b927cc23bb4921efadfac83ae664ea9
Razzaru <ruck2424@mail.ru>
2017-02-05 21:38:52 +0300

    github api service bind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { ProfileComponent } from './components/profile.component';
+import { NavbarComponent } from './components/navbar.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, HttpModule ],
+  declarations: [ AppComponent, ProfileComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+  private username: string;
+  private client_id = '5aacf38914229eac456b';
+  private client_secret = '24d020df6ef0e3000393dd56d7aa2d0090027be3';
+
+  constructor(private _http: Http) {
+    console.log('Github Service Ready...');
+    this.username = 'Razzaru';
+  }
+
+  getUser() {
+    return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret).map(res => res.json());
+  }
+}
